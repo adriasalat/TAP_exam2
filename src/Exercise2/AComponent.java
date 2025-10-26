433f12654d61c31618d4b5c026d04dbe7fe93b8e
Adria Salat Ripoll <salatripoll@gmail.com>
2025-10-26 11:49:54 +0100

    Files for the exercise 2

---
+++ src/Exercise2/AComponent.java
@@ -0,0 +1,13 @@
+package Exercise2;
+
+import java.util.List;
+
+ interface AComponent {
+	 void ls();
+	 List<String> collect();
+	 List<AComponent> toList();
+	 List<File> search(String name);
+	 int size();
+	 void setParent(AComponent parent);
+	 String getName();
+}
--- src/Exercise2/App.java
+++
@@ -1,5 +0,0 @@
-package Exercise2;
-
-public class App {
-    
-}
